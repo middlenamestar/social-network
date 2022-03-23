a4e1a147939cbf42802345ce0e4ac2de9c030582
priscilla ma <priscillama621@gmail.com>
2022-03-23 19:23:48 -0400

    added user controller

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const user = require('../models/user');
+
+module.exports = {
+    getUsers(req, res){
+        user.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    getOneUser(req, res){
+        user.findOne({_id: req.params.userId})
+            .select('-__v') //excludes version
+            .then((user) =>
+                !user
+                    ? res.status(404).json({message: 'no user w that id.'})
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    createUser(req, res){
+        user.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res){
+        user.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+            .then((user) => 
+                !user
+                    ? res.status(404).json({message: 'no user w this id.'})
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteUser(req, res){
+        user.findOneAndDelete({_id: req.params.userId})
+            .then((user) =>
+                !user
+                    ? res.status(404).json({message: 'no user w that id.'})
+                    : thought.deleteMany({_id: {$in: user.thoughts}})
+            )
+            .then(() => res.json({message: 'user and associated thoughts deleted.'}))
+            .catch((err) => res.status(500).json(err));
+    }
+};
