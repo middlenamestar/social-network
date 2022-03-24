b767bc7c62578b765960cdc87781737f73a5b553
priscilla ma <priscillama621@gmail.com>
2022-03-24 16:36:36 -0400

    changed connection string

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect(connectionString, {
+connect('mongodb://localhost/socialNetwork', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
