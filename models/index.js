a922d541268dded28a40cdf57c0dd2802f061647
priscilla ma <priscillama621@gmail.com>
2022-03-25 00:00:40 -0400

    deleted reaction js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 const user = require('./user');
 const thought = require('./thought');
-const reaction = require('./reaction');
 
-module.exports = {user, thought, reaction};
+module.exports = { user, thought };
